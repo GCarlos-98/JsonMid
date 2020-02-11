6e4e0b6a42697d936f84aecaa6ae9ade8d9a6292
GCarlos-98 <60873808+GCarlos-98@users.noreply.github.com>
2020-02-11 22:56:46 +0800

    Add files via upload

---
+++ Decode.php
@@ -0,0 +1,26 @@
+<?php
+// header('Content-Type: application/json');
+
+$db_username = 'admin';
+$db_password = 'root1234';
+$db_hostname = 'dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com';
+$db_port = '3306';
+$db_name = 'db_1821709';
+
+$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+$sql = "SELECT * FROM Tb1";
+$row = mysqli_query($conn, $sql);
+
+while ($item = mysqli_fetch_object($row)) {
+    $json = json_encode($item);
+	$dec = json_decode($json, true);
+
+	echo "<ul>";
+	echo "<li>". $dec["DeltaV"] ."</li>";
+	echo "<ul>";
+	echo "<li>". $dec["System"]." </li>";
+	echo "<li>". $dec["Name"] ."</li>";
+	echo "<li>". $dec["Orbit"] ."</li>";
+	echo "</ul>";
+	echo "</ul>";
+}
